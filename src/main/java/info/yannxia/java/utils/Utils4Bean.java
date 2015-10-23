dc319ba8e11c26064ddf05072ec3296524f270a6
yann <yann@yanndeMacBook-Pro-2.local>
2015-10-23 14:06:58 +0800

    add depend, add one function

--- src/main/java/info/yannxia/java/utils/Utils4Bean.java
+++ src/main/java/info/yannxia/java/utils/Utils4Bean.java
@@ -11,6 +11,15 @@ import java.util.Arrays;
 public class Utils4Bean {
     private final Logger logger = LoggerContext.getContext().getLogger(Utils4Bean.class.getName());
 
+    /**
+     * most of time. if pass one param to it, will more quick
+     * @param o object
+     * @return true is null, false is not null
+     */
+    public static boolean isNotNull(Object o) {
+        return o == null;
+    }
+
     public static boolean isNotNull(Object o, Object... os) {
         return !Arrays.asList(o, os).stream().anyMatch((i) -> i == null);
     }
